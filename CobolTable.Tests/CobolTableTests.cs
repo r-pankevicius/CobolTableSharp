be486c46dec51378aad64f5382a9d56bcc333770
Remigijus Pankevičius <r-pankevicius@users.noreply.github.com>
2020-10-14 10:47:37 +0300

    Ported to .NET Standard

--- CobolTable.Tests/CobolTableTests.cs
+++ CobolTable.Tests/CobolTableTests.cs
@@ -1,14 +1,13 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-using Shouldly;
+﻿using Shouldly;
 using System.Data;
 using System.Linq;
+using Xunit;
 
 namespace spartan.COBOL
 {
-	[TestClass]
 	public class CobolTableTests
 	{
-		[TestMethod]
+		[Fact]
 		public void CobolTable_AddRow_AddsCorrectNumberOfRows()
 		{
 			var table = new CobolTable<MyRow>();
@@ -19,10 +18,10 @@ namespace spartan.COBOL
 			var row2 = new MyRow() { A = 1000, B = 1000 };
 			table.AddRow(row2);
 
-			Assert.AreEqual(2, table.Rows.Count, "Incorrect table.Rows.Count");
+			table.Rows.Count.ShouldBe(2, "Incorrect table.Rows.Count");
 		}
 
-		[TestMethod]
+		[Fact]
 		public void CobolTable_SumAllTableFields()
 		{
 			var table = new CobolTable<MyRow>();
@@ -38,10 +37,10 @@ namespace spartan.COBOL
 			foreach (MyRow row in table.Rows)
 				tableTotal += row.A + row.B;
 
-			Assert.AreEqual(2012, tableTotal, "Incorrect tableTotal");
+			tableTotal.ShouldBe(2012, "Incorrect tableTotal");
 		}
 
-		[TestMethod]
+		[Fact]
 		public void CobolTable_WrapsExistingTable_WithSameColumnNamesAsPropertyNames()
 		{
 			var dataTable = new System.Data.DataTable();
@@ -56,86 +55,86 @@ namespace spartan.COBOL
 
 			var typedTable = new CobolTable<MyRow>(dataTable);
 
-			Assert.AreEqual(1, typedTable.Rows.Count, "Incorrect number of rows");
+			typedTable.Rows.Count.ShouldBe(1, "Incorrect number of rows");
 
 			// Check first row
 			MyRow row = typedTable.Rows[0];
-			Assert.AreEqual(1, row.A, "Incorrect rows[0].A");
-			Assert.AreEqual(2, row.B, "Incorrect rows[0].B");
+			row.A.ShouldBe(1, "Incorrect rows[0].A");
+			row.B.ShouldBe(2, "Incorrect rows[0].B");
 
 			// Assign new value vie DataRow
 			dataTable.Rows[0]["A"] = 10;
-			Assert.AreEqual(10, typedTable.Rows[0].A, "Incorrect rows[0].A after assignment");
+			typedTable.Rows[0].A.ShouldBe(10, "Incorrect rows[0].A after assignment");
 		}
 
-		[TestMethod]
+		[Fact]
 		public void CobolTable_CreatesTableWithPrimaryKey()
 		{
 			var persons = new Person[]
 			{
-				new Person() { ID = 1, Name = "Mike", Surname = "Bloomberg" },
-				new Person() { ID = 2, Name = "Chose", Surname = "Pedro" }
+				new Person { ID = 1, Name = "Mike", Surname = "Bloomberg" },
+				new Person { ID = 2, Name = "Chose", Surname = "Pedro" }
 			};
 
-			CobolTable<Person> typedTable = CobolTable.Create(persons, "ID");
+			var typedTable = CobolTable.Create(persons, "ID");
 
-			Assert.AreEqual(2, typedTable.Rows.Count, "Incorrect number of rows");
+			typedTable.Rows.Count.ShouldBe(2, "Incorrect number of rows");
 
 			// Check column names
 			DataTable table = typedTable.DataTable;
-			Assert.AreEqual(3, table.Columns.Count, "Incorrect column count");
-			Assert.AreEqual("ID", table.Columns[0].ColumnName, "Incorrect name for column 0");
-			Assert.AreEqual("Name", table.Columns[1].ColumnName, "Incorrect name for column 1");
-			Assert.AreEqual("Surname", table.Columns[2].ColumnName, "Incorrect name for column 2");
+			table.Columns.Count.ShouldBe(3, "Incorrect column count");
+			table.Columns[0].ColumnName.ShouldBe("ID", "Incorrect name for column 0");
+			table.Columns[1].ColumnName.ShouldBe("Name", "Incorrect name for column 1");
+			table.Columns[2].ColumnName.ShouldBe("Surname", "Incorrect name for column 2");
 
 			// Check primary key
-			Assert.AreEqual(1, table.PrimaryKey.Count(), "Incorrect PK columns count");
-			Assert.AreEqual("ID", table.PrimaryKey[0].ColumnName, "Incorrect PK column name");
+			table.PrimaryKey.Count().ShouldBe(1, "Incorrect PK columns count");
+			table.PrimaryKey[0].ColumnName.ShouldBe("ID", "Incorrect PK column name");
 
 			// Check first row
 			Person row0 = typedTable.Rows[0];
-			Assert.AreEqual(1, row0.ID, "Incorrect rows[0].ID");
-			Assert.AreEqual("Mike", row0.Name, "Incorrect rows[0].Name");
-			Assert.AreEqual("Bloomberg", row0.Surname, "Incorrect rows[0].Surname");
+			row0.ID.ShouldBe(1, "Incorrect rows[0].ID");
+			row0.Name.ShouldBe("Mike", "Incorrect rows[0].Name");
+			row0.Surname.ShouldBe("Bloomberg", "Incorrect rows[0].Surname");
 
 			// Check second row
 			Person row1 = typedTable.Rows[1];
-			Assert.AreEqual(2, row1.ID, "Incorrect rows[1].ID");
-			Assert.AreEqual("Chose", row1.Name, "Incorrect rows[1].Name");
-			Assert.AreEqual("Pedro", row1.Surname, "Incorrect rows[1].Surname");
+			row1.ID.ShouldBe(2, "Incorrect rows[1].ID");
+			row1.Name.ShouldBe("Chose", "Incorrect rows[1].Name");
+			row1.Surname.ShouldBe("Pedro", "Incorrect rows[1].Surname");
 		}
 
-		[TestMethod]
+		[Fact]
 		public void CobolTable_WorksWithNullableTypes()
 		{
 			var persons = new RowWithNullables[]
 			{
-				new RowWithNullables() { N = 1, F = null },
-				new RowWithNullables() { N = null, F = 1.0 }
+				new RowWithNullables { N = 1, F = null },
+				new RowWithNullables { N = null, F = 1.0 }
 			};
 
-			CobolTable<RowWithNullables> typedTable = CobolTable.Create(persons, "ID");
+			var typedTable = CobolTable.Create(persons, "ID");
 
-			Assert.AreEqual(2, typedTable.Rows.Count, "Incorrect number of rows");
+			typedTable.Rows.Count.ShouldBe(2, "Incorrect number of rows");
 
 			// Check column names
 			DataTable table = typedTable.DataTable;
-			Assert.AreEqual(2, table.Columns.Count, "Incorrect column count");
-			Assert.AreEqual("N", table.Columns[0].ColumnName, "Incorrect name for column 0");
-			Assert.AreEqual("F", table.Columns[1].ColumnName, "Incorrect name for column 1");
+			table.Columns.Count.ShouldBe(2, "Incorrect column count");
+			table.Columns[0].ColumnName.ShouldBe("N", "Incorrect name for column 0");
+			table.Columns[1].ColumnName.ShouldBe("F", "Incorrect name for column 1");
 
 			// Check first row
 			RowWithNullables row0 = typedTable.Rows[0];
-			Assert.AreEqual(1, row0.N, "Incorrect rows[0].N");
-			Assert.AreEqual(null, row0.F, "Incorrect rows[0].F");
+			row0.N.ShouldBe(1, "Incorrect rows[0].N");
+			row0.F.ShouldBeNull("Incorrect rows[0].F");
 
 			// Check second row
 			RowWithNullables row1 = typedTable.Rows[1];
-			Assert.AreEqual(null, row1.N, "Incorrect rows[1].N");
-			Assert.AreEqual(1.0, row1.F, "Incorrect rows[1].F");
+			row1.N.ShouldBeNull("Incorrect rows[1].N");
+			row1.F.ShouldBe(1.0, "Incorrect rows[1].F");
 		}
 
-		[TestMethod]
+		[Fact]
 		public void CobolTable_ConvertRow_Works()
 		{
 			var typedTable = new CobolTable<Person>();
@@ -147,13 +146,13 @@ namespace spartan.COBOL
 
 			Person person = typedTable.ConvertRow(row);
 
-			Assert.IsNotNull(person);
-			Assert.AreEqual(123, person.ID, "Incorrect person.ID");
-			Assert.AreEqual("Bob", person.Name, "Incorrect person.Name");
-			Assert.AreEqual(null, person.Surname, "Incorrect person.Surname");
+			person.ShouldNotBeNull();
+			person.ID.ShouldBe(123, "Incorrect person.ID");
+			person.Name.ShouldBe("Bob", "Incorrect person.Name");
+			person.Surname.ShouldBeNull("Incorrect person.Surname");
 		}
 
-		[TestMethod]
+		[Fact]
 		public void CobolTable_GetFieldNames()
 		{
 			string[] fieldNames = CobolTable.GetFieldNames<MyRow>();
